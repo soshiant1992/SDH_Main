33f03d81c09bd6c89fe5d1379088c575798a9274
Mohammadjavad Davari Dolatabadi <esoshiantd@gmail.com>
2018-05-30 12:46:11 -0700

    cleanup

--- src/ramp_gripper/sdh_grasp/src/sdh_grasp_control.cpp
+++ src/ramp_gripper/sdh_grasp/src/sdh_grasp_control.cpp
@@ -17,6 +17,10 @@ int main(int argc, char **argv)
      init = NH.serviceClient<std_srvs::Trigger>("/driver/init");
      recover=NH.serviceClient<std_srvs::Trigger>("/driver/recover");
      stop=NH.serviceClient<std_srvs::Trigger>("/driver/stop");
+     traj_client = new SimpleActionClient<control_msgs::FollowJointTrajectoryAction>("/joint_trajectory_controller/follow_joint_trajectory",true);
+     set_velocity = NH.advertise<std_msgs::Float64MultiArray> ("/joint_group_velocity_controller/command", 1);
+     set_operation_mode_client = NH.serviceClient<cob_srvs::SetString>("/driver/set_operation_mode");
+
     std_srvs::Trigger triger ;
     vel_dim.data.resize(7);
     vel_dim.data[0] = 0 ;
@@ -26,41 +30,21 @@ int main(int argc, char **argv)
         ROS_ERROR("Failed to call service driver/init");
     }
 
-    //    threshold[0] = threshold[1] = 500;
-    //    threshold[2] = threshold[3] = 2;
-    //    threshold[4] = threshold[5] = 100;
 
-//    threshold[0] = threshold[1] = 20;
-//    threshold[2] = threshold[3] = 20;
-//    threshold[4] = threshold[5] = 20;
-
-//    ros::NodeHandle v ("~");
-
-    set_operation_mode_client = NH.serviceClient<cob_srvs::SetString>("/driver/set_operation_mode");
-    ros::Subscriber sub = NH.subscribe("/joint_states", 1, jointCallback);
-    ros::Subscriber sub1 = NH.subscribe("/dsa_controller/tactile_data", 1, tactileCallback);
-    ros::Subscriber sub2 = NH.subscribe("/backoff", 1, backoff);
-   // ros::Subscriber sub3 = NH.subscribe("/joint_trajectory_controller/follow_joint_trajectory/result", 1, keepbackingoff);
-
-    set_velocity = NH.advertise<std_msgs::Float64MultiArray> ("/joint_group_velocity_controller/command", 1);
+    ros::Subscriber joint_states = NH.subscribe("/joint_states", 1, jointCallback);
+    ros::Subscriber tactile_data = NH.subscribe("/dsa_controller/tactile_data", 1, tactileCallback);
+    ros::Subscriber backoffsub = NH.subscribe("/backoff", 1, backoff);
     ros::Subscriber pre_grasp_pos = NH.subscribe("/sdh_grasp_control/pre_grasp_config" , 1 , grasp_pos_callback);
     ros::Subscriber grasp_type  = NH.subscribe("/sdh_grasp_control/grasp_goal" , 1 , grasp_type_callback);
     ros::Subscriber stop_it  = NH.subscribe("/stop_it" , 1 , stop_callback);
-    //    set_feedback = NH.advertise<std_msgs::Int8>("/sdh_grasp_control/status" , 1);
-    //  ros::spin();
-    //    ros::Rate loop_rate(1);
 
-    //    setvelocitymode();
     ros::Rate loop_rate(100);
 begin_t= ros::Time::now().toSec();
-
+ros::AsyncSpinner spinner(4); // Use 4 threads
+    spinner.start();
     while(ros::ok()) {
-
-
-
-
-        ros::spinOnce();
+//        ros::spinOnce();
         loop_rate.sleep();
     }
-    return 0;
+
 }
